bd338d0feed337b127b59aa32c64b4515adb78c9
steven chen <schen0096@gmail.com>
2021-07-06 17:55:29 -0400

    Completed Assignment and added remove button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  document.querySelector('#create-task-form').addEventListener("submit", (e) => {
+    e.preventDefault()
+    let li = document.createElement('li')
+    let btn = document.createElement('button')
+    btn.type = 'remove'
+    btn.id = 'removeLi'
+    btn.textContent = 'X'
+    
+    btn.addEventListener('click', () => {
+      li.remove()
+    })
+
+    li.textContent = e.target['new-task-description'].value
+    li.append(btn)
+    document.querySelector('#tasks').appendChild(li)
+    })
+})
+
